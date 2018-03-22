995ff121c9bc82b88b45e8d0ac46098dd9de5bf4
Joel Dueck <joel@jdueck.net>
2018-03-22 12:42:02 -0500

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define collection "bookcover")
+(define version "0.1.0")
+
+(define deps '("base"
+               "beautiful-racket-lib"
+               "draw-lib"
+               "pdf-read"
+               "pict-lib"))
