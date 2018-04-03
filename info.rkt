6e09b569d255ef4ede5b5848a7960093472347a8
Joel Dueck <joel@jdueck.net>
2018-04-02 22:34:58 -0500

    Add scribblings to info.rkt

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "bookcover")
 (define version "0.1.0")
-
+(define scribblings '(("scribblings/bookcover.scrbl" (multi-page))))
 (define deps '("base"
                "beautiful-racket-lib"
                "draw-lib"
