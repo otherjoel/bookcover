93106e2a0388c0be693e49825b8df5d47f5ca811
Joel Dueck <joel@jdueck.net>
2018-04-08 19:23:08 -0500

    Tweak indents

--- scribblings/example-cover.rkt
+++ scribblings/example-cover.rkt
@@ -3,12 +3,12 @@
 (setup #:interior-pdf "my-book.pdf"
        #:cover-pdf "example-cover.pdf")
 
-(define title    (colorize (text "Sunshine for Sal" (cons 'bold "Times New Roman") 48) "blue"))
+(define title (colorize (text "Sunshine for Sal" (cons 'bold "Times New Roman") 48) "blue"))
 (define subtitle (text "A Painfully Verbose Memoir" (cons 'italic "Times New Roman") 26))
-(define accent   (filled-rectangle (coverwidth)
-                                   (/ (pageheight) 3)
-                                   #:color "LemonChiffon"
-                                   #:draw-border? #f))
+(define accent (filled-rectangle (coverwidth)
+                                 (/ (pageheight) 3)
+                                 #:color "LemonChiffon"
+                                 #:draw-border? #f))
 
 (cover-draw accent 0 (/ (pageheight) 3))
 (frontcover-draw title #:top (* (pageheight) 2/6) #:horiz-center? #t)
