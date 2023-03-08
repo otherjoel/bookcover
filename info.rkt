870b54e86e0bc2b01da51828a1ccb2fb2a605544
Joel Dueck <joel@jdueck.net>
2023-03-08 08:40:48 -0600

    Fix version number format
    Surfaced by racket/racket#4588

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "bookcover")
-(define version "0.1.0")
+(define version "0.1")
 (define scribblings '(("scribblings/bookcover.scrbl" (multi-page))))
 (define deps '("base"
                "beautiful-racket-lib"
